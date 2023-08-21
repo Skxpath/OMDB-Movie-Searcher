35ec0fb6e5b7a1a28d59c5e45560405eb5424899
Alex <theundeadpath@gmail.com>
2023-08-20 21:10:46 -0700

    updated search bar to search instantly on submit, and refactored strings out as const variables

--- src/Components/SearchBar.tsx
+++ src/Components/SearchBar.tsx
@@ -3,13 +3,17 @@ import { useState, useEffect, useCallback } from 'react';
 import { MovieProps, OmdbSearchParameters, OmdbSearchResults } from '../Types/Types';
 import config from "../config.json";
 
-const createSearchParameters = (movieName: string, movieYear: string): OmdbSearchParameters => (
-    {
-        Title: movieName,
-        Type: 'movie',
-        ...(movieYear ? { Year: movieYear } : {})
-    }
-);
+const WAITING_FOR_SEARCH: string = "Waiting for search...";
+const SEARCHING: string = "Searching...";
+const SEARCH_SUCCESSFUL: string = "Search successful! Total results found: "; 
+const ERROR_SEARCHING: string = "Error searching movie info: ";
+const CLEARED_SEARCH: string = "Cleared search results";
+const MOVIE_NAME_PLACEHOLDER: string = "Search movie by name";
+const MOVIE_YEAR_PLACEHOLDER: string = "Enter movie year (optional)";
+
+const TYPE_MOVIE: string = "movie";
+
+let getData: NodeJS.Timeout;
 
 type SearchBarProps = {
     setMovieInfo: React.Dispatch<React.SetStateAction<MovieProps[]>>
@@ -18,7 +22,7 @@ type SearchBarProps = {
 function SearchBar({ setMovieInfo }: SearchBarProps) {
     const [movieName, setMovieName] = useState("");
     const [movieYear, setMovieYear] = useState("");
-    const [searchStatusText, setSearchStatusText] = useState("Waiting for search...");
+    const [searchStatusText, setSearchStatusText] = useState(WAITING_FOR_SEARCH);
 
     const debounceSearchDelay: number = config.debounceSearchDelay || 2000;
 
@@ -37,45 +41,64 @@ function SearchBar({ setMovieInfo }: SearchBarProps) {
 
         if (result.Response === "True" && result.Search) {
             setMovieInfo(result.Search);
-            setSearchStatusText("Search successful! Total results found: " + result.totalResults);
+            setSearchStatusText(SEARCH_SUCCESSFUL + result.totalResults);
         } else {
-            setSearchStatusText("Error searching movie info - " + result.Error);
+            setSearchStatusText(ERROR_SEARCHING + result.Error);
         }
     }, [movieName, movieYear, setMovieInfo]);
 
+    const createSearchParameters = (movieName: string, movieYear: string): OmdbSearchParameters => (
+        {
+            Title: movieName,
+            Type: TYPE_MOVIE,
+            ...(movieYear ? { Year: movieYear } : {})
+        }
+    );
+
     const clearSearchResults = () => {
         setMovieInfo([]);
-        setSearchStatusText("Cleared search results");
+        setSearchStatusText(CLEARED_SEARCH);
         setMovieName("");
         setMovieYear("");
     }
 
     useEffect(() => {
-        const getData = setTimeout(() => {
+         getData = setTimeout(() => {
             if (movieName) {
+                setSearchStatusText(SEARCHING);
                 searchForMovies();
             } else {
-                setSearchStatusText("Waiting for search...");
+                setSearchStatusText(WAITING_FOR_SEARCH);
             }
         }, debounceSearchDelay)
 
         return () => clearTimeout(getData);
     }, [movieName, movieYear, searchForMovies, debounceSearchDelay])
 
+    const onSubmitMovieSearch = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        clearTimeout(getData);
+        setSearchStatusText(SEARCHING);
+        searchForMovies();
+    }
+
     return (
         <div>
+        <form onSubmit={onSubmitMovieSearch}>
             <input
                 type="movieName"
-                placeholder="Search movie by name"
+                placeholder={MOVIE_NAME_PLACEHOLDER}
                 onChange={handleMovieNameChange}
                 value={movieName} />
             <input
                 type="movieYear"
-                placeholder="Enter movie year (optional)"
+                placeholder={MOVIE_YEAR_PLACEHOLDER}
                 onChange={handleMovieYearChange}
                 value={movieYear} />
+            <input type="submit" hidden />
+        </form>
             <button onClick={clearSearchResults}>Clear</button>
-            <p>{searchStatusText}</p>
+            <p>{searchStatusText}</p>         
         </div>
     );
 }
